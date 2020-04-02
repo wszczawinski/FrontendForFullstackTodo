3e315f716c15646fca9efb1c13f0b20aa9069ad4
wszczawinski <w.k.szczawinski@gmail.com>
2020-04-02 23:10:02 +0200

    feat(fetchAPI): loading initial data from API

---
+++ src/TodoList.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+const APIURL = '/api/todos';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+
+  componentWillMount() {
+    this.loadTodos();
+  }
+
+  loadTodos() {
+    fetch(APIURL)
+      .then(resp => {
+        if (!resp.ok) {
+          if (resp.status >= 400 && resp.status < 500) {
+            return resp.json().then(data => {
+              let err = { errorMessage: data.message };
+              throw err;
+            });
+          } else {
+            let err = {
+              errorMessage:
+                'Please try later, server is not responding :('
+            };
+            throw err;
+          }
+        }
+        return resp.json();
+      })
+      .then(todos => this.setState({ todos }));
+  }
+
+  render() {
+    return <h1>Todo List</h1>;
+  }
+}
+
+export default TodoList;
